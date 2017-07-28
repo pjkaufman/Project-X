51b3434d74aa156b12f2e202b1e3c340fc2e6383
Peter Kaufman <Peter@RevenFlo.com>
2017-07-27 20:49:56 -0700

    Updated the Versions page. Functions are still lacking.

--- assets/versions/js/versions.js
+++ assets/versions/js/versions.js
@@ -6,12 +6,17 @@ $( document ).ready(function() {
     success: function(resp){
       var markup;
        for(row in resp){
-         markup = '<div class="row" style="margin:10px;"><label class="col-sm-4" style="width:30%;">' + resp[row]['name'] + ' Version: </label>' +
+
+         markup = '<div class="row" style="margin:10px;"><label class="col-sm-4" style="width:30%;">' + firstLetterToUpper(resp[row]['name']) + ' Version: </label>' +
                   '<input class="col-sm-6" id="' + resp[row]['name'] + '" type="text"  placeholder="' + resp[row]['version'] + '">' +
-                  '<button class="col-sm-2" id="update_' + resp[row]['name'] + '" style="width:auto;">Update</button></div>';
+                  '<div class="col-sm-1"></div><button class="col-sm-1" id="update_' + resp[row]['name'] + '" style="width:auto;">Update</button></div>';
 
          $('div#versions').append(markup);
        };
     },
   });
 });
+
+function firstLetterToUpper(str){
+  return str.substring(0,1).toUpperCase() + str.substring(1);
+}
