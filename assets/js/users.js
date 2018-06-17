777ee27e6ac188cc11d50bd00d7c9f935998bded
pjkaufman <PeterJamesKaufman@gmail.com>
2018-06-16 22:50:24 -0400

    Updated folder structure

--- assets/js/users.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-$(document).ready(function() {
-    Table.init('users', location + '/getUserData', ['ID', 'Username', 'Email', 'Created At', 'Status', 'Last Login', 'Number of Logins'], false);
-});
